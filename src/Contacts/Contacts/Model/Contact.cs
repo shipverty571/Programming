77e4472f530d9505e44b9b9209615bc67cf25aca
Валентин Борнашов <valentin1741@gmail.com>
2023-03-06 23:49:20 +0700

    feature(Contact, MainWindow): реализован класс Contact и проделана верстка MainWindow

---
+++ src/Contacts/Contacts/Model/Contact.cs
@@ -0,0 +1,59 @@
+﻿namespace Contacts.Model
+{
+    public class Contact
+    {
+        private string _name;
+
+        private string _email;
+
+        private string _phone;
+
+        public Contact(string name, string email, string phone)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+
+        public Contact()
+        {
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return _phone;
+            }
+            set
+            {
+                _phone = value;
+            }
+        }
+    }
+}
