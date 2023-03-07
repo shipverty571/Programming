74647741407638a6a10c8c95db931557665688c4
Валентин Борнашов <valentin1741@gmail.com>
2023-03-07 11:43:38 +0700

    feature(MainVM): реализованы команды SaveCommand и LoadCommand

---
+++ src/Contacts/Contacts/ViewModel/LoadCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Contacts.ViewModel
+{
+    public class LoadCommand : ICommand
+    {
+        private Action<object> execute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public LoadCommand(Action<object> execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
