1044df6234695425f1862e3b5a855666e36ca0fc
Валентин Борнашов <shipverty@vk.com>
2023-04-06 12:44:31 +0700

    feature(Contacts): очищен проект для новой реализации

--- src/Contacts/Contacts/ViewModel/MainVM.cs
+++ src/Contacts/Contacts/ViewModel/MainVM.cs
@@ -1,121 +1,25 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Windows.Input;
-using Contacts.Model;
-using Contacts.Model.Services;
 
 namespace Contacts.ViewModel
 {
     /// <summary>
-    /// ViewModel для окна MainWindow.
+    ///     ViewModel для окна MainWindow.
     /// </summary>
     public class MainVM : INotifyPropertyChanged
     {
         /// <summary>
-        /// Контакт.
-        /// </summary>
-        public Contact Contact { get; private set; } = new Contact();
-
-        /// <summary>
-        /// Возвращает и задаёт путь сериализации. По умолчанию - папка "Мои документы".
-        /// </summary>
-        public string Path { get; set; } = 
-            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
-            + @"\Contacts\contacts.json";
-
-        /// <summary>
-        /// Возвращает и задаёт имя контакта.
-        /// </summary>
-        public string Name
-        {
-            get
-            {
-                return Contact.Name;
-            }
-            set
-            {
-                Contact.Name = value;
-                OnPropertyChanged(nameof(Name));
-            }
-        }
-
-        /// <summary>
-        /// Возвращает и задаёт электронную почту контакта.
-        /// </summary>
-        public string Email
-        {
-            get
-            {
-                return Contact.Email;
-            }
-            set
-            {
-                Contact.Email = value;
-                OnPropertyChanged(nameof(Email));
-            }
-        }
-
-        /// <summary>
-        /// Возвращает и задаёт номер телефона контакта.
-        /// </summary>
-        public string PhoneNumber
-        {
-            get
-            {
-                return Contact.Phone;
-            }
-            set
-            {
-                Contact.Phone = value;
-                OnPropertyChanged(nameof(PhoneNumber));
-            }
-        }
-
-        /// <summary>
-        /// Команда сериализации контакта.
-        /// </summary>
-        public ICommand SaveCommand
-        {
-            get
-            {
-                return new RelayCommand((obj) =>
-                {
-                    ContactSerializer.Serialize(Contact, Path);
-                });
-            }
-        }
-
-        /// <summary>
-        /// Команда десериализации контакта.
-        /// </summary>
-        public ICommand LoadCommand
-        {
-            get
-            {
-                return new RelayCommand((obj) =>
-                {
-                    var contact = ContactSerializer.Deserialize(Path);
-                    Name = contact.Name;
-                    Email = contact.Email;
-                    PhoneNumber = contact.Phone;
-                });
-            }
-        }
-
-        /// <summary>
-        /// Событие изменения свойства.
+        ///     Событие изменения свойства.
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
-        /// При вызове зажигает событие <see cref="PropertyChanged"/>.
+        ///     При вызове зажигает событие <see cref="PropertyChanged" />.
         /// </summary>
-        /// <param name="prop">Имя свойства, вызвавшего метод.</param>
-        protected void OnPropertyChanged([CallerMemberName] string prop = "")
+        /// <param name="propertyName">Имя свойства, вызвавшего метод.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
