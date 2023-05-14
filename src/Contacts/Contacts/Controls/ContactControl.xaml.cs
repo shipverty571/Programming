a0cd6e7c194ffa08854233b21691c8440eb4cc3c
Валентин Борнашов <shipverty@vk.com>
2023-05-14 12:19:39 +0700

    feature(ContactControl): правая часть верстки вынесена в контрол и установлена связь между формой и контролом

---
+++ src/Contacts/Contacts/Controls/ContactControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Contacts.ViewModel;
+
+namespace Contacts.Controls
+{
+    public partial class ContactControl : UserControl
+    {
+        public ContactVM SelectedContact
+        {
+            get => (ContactVM)GetValue(SelectedContactProperty);
+            set => SetValue(SelectedContactProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedContactProperty =
+            DependencyProperty.Register("SelectedContact", typeof(ContactVM),
+                typeof(ContactControl), new UIPropertyMetadata(null));
+            
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
