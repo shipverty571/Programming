9f0ec34889e586a61cf7d8d0d316e6699f799ef5
Валентин Борнашов <shipverty@vk.com>
2023-05-14 15:34:03 +0700

    feature(ContactVM): реализована валидация для полей ввода

--- src/Contacts/Contacts/ViewModel/ContactVM.cs
+++ src/Contacts/Contacts/ViewModel/ContactVM.cs
@@ -3,9 +3,13 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Contacts.Model;
+using Contacts.Model.Services;
 
 namespace Contacts.ViewModel
 {
+    /// <summary>
+    /// ViewModel для Contact.
+    /// </summary>
     public class ContactVM : ObservableValidator, ICloneable
     {
         /// <summary>
@@ -25,6 +29,7 @@ namespace Contacts.ViewModel
         /// <summary>
         /// Возвращает и задаёт имя контакта.
         /// </summary>
+        [CustomValidation(typeof(ContactVM), nameof(ValidateName))]
         public string Name
         {
             get => Contact.Name;
@@ -32,12 +37,14 @@ namespace Contacts.ViewModel
                 Contact.Name,
                 value,
                 Contact,
-                (contact, name) => Contact.Name = name);
+                (contact, name) => Contact.Name = name,
+                true);
         }
 
         /// <summary>
         /// Возвращает и задаёт электронную почту контакта.
         /// </summary>
+        [CustomValidation(typeof(ContactVM), nameof(ValidateEmail))]
         public string Email
         {
             get => Contact.Email;
@@ -45,13 +52,13 @@ namespace Contacts.ViewModel
                 Contact.Email,
                 value,
                 Contact,
-                (contact, email) => Contact.Email = email);
+                (contact, email) => Contact.Email = email,
+                true);
         }
 
         /// <summary>
         /// Возвращает и задаёт номер телефона контакта.
         /// </summary>
-        [Required]
         [CustomValidation(typeof(ContactVM), nameof(ValidatePhone))]
         public string Phone
         {
@@ -77,7 +84,7 @@ namespace Contacts.ViewModel
         /// Валидация номера телефона.
         /// </summary>
         /// <param name="phone">Номер телефона.</param>
-        /// <returns>Возвращает результат валидации.</returns>
+        /// <returns>Возвращает результат валидации <see cref="ValidationResult"/>.</returns>
         public static ValidationResult ValidatePhone(string phone)
         {
             var pattern = @"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$";
@@ -85,6 +92,32 @@ namespace Contacts.ViewModel
                 return ValidationResult.Success;
 
             return new ValidationResult("The phone number is not correctly.");
+        }
+        /// <summary>
+        /// Валидация адреса электронной почты.
+        /// </summary>
+        /// <param name="email">Электронная почта.</param>
+        /// <returns>Возвращает результат валидации <see cref="ValidationResult"/>.</returns>
+        public static ValidationResult ValidateEmail(string email)
+        {
+            var pattern = @"^\w+\@{1}\w+\.{1}\w+";
+            if (Regex.IsMatch(email, pattern))
+                return ValidationResult.Success;
+
+            return new ValidationResult("The email is not correctly.");
+        }
+        
+        /// <summary>
+        /// Валидация имени.
+        /// </summary>
+        /// <param name="name">Имя.</param>
+        /// <returns>Возвращает результат валидации <see cref="ValidationResult"/>.</returns>
+        public static ValidationResult ValidateName(string name)
+        {
+            if (name.Length > 0 && name.Length <= 100)
+                return ValidationResult.Success;
+
+            return new ValidationResult("The name is not correctly.");
         }
     }
 }
