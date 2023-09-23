4377efa8ae19e5846c812dcbc26edeca64e27f4f
Валентин Борнашов <shipverty@vk.com>
2023-09-23 23:57:37 +0700

    feature(CreateNoteViewModel): добавил модель представления для добавления и редактирования заметок

---
+++ src/NoteApp/NoteApp/Domain/ViewModels/CreateNoteViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace NoteApp.Domain.ViewModels;
+
+public class CreateNoteViewModel
+{
+    /// <summary>
+    /// Возвращает и задает заголовок.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Возвращает и задает описание.
+    /// </summary>
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Возвращает и задает категорию.
+    /// </summary>
+    public string Category { get; set; }
+}
