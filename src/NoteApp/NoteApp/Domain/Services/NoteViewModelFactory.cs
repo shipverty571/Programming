902f3257739cbd5802d71ec85e35ce58b3f6a31c
Валентин Борнашов <shipverty@vk.com>
2023-09-24 00:00:33 +0700

    feature(NoteViewModelFactory): изменил реализацию класса под бд

--- src/NoteApp/NoteApp/Domain/Services/NoteViewModelFactory.cs
+++ src/NoteApp/NoteApp/Domain/Services/NoteViewModelFactory.cs
@@ -1,4 +1,5 @@
-﻿using NoteApp.Domain.ViewModels;
+﻿using NoteApp.DAL.Interfaces;
+using NoteApp.Domain.ViewModels;
 using NoteApp.TestValues;
 
 namespace NoteApp.Domain.Services
@@ -6,16 +7,24 @@ namespace NoteApp.Domain.Services
     /// <summary>
     /// Сервисный класс для создания объектов <see cref="NoteViewModel"/>.
     /// </summary>
-    public static class NoteViewModelFactory
+    public class NoteViewModelFactory
     {
+        private readonly INoteRepository _noteRepository;
+
+        public NoteViewModelFactory(INoteRepository noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
         /// <summary>
         /// Создает объект <see cref="NoteViewModel"/>.
         /// </summary>
         /// <param name="noteId">Уникальный идентификатор заметки.</param>
         /// <returns>Возвращает объект <see cref="NoteViewModel"/>.</returns>
-        public static NoteViewModel Create(int noteId)
+        public NoteViewModel Create(int noteId)
         {
-            var note = TestData.Notes.First(x => x.Id == noteId);
+            var allNotes = _noteRepository.GetAll();
+            var note = allNotes.First(x => x.Id == noteId);
             return new NoteViewModel
             {
                 Name = note.Name,
