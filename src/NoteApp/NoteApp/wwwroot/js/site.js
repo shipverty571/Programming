2ed0be079c6737aab7369a00f6b291f60852d21b
Валентин Борнашов <shipverty@vk.com>
2023-09-23 14:23:04 +0700

    refactor(_Layout.cshtml, Index.cshtml, site.js): Вынес скрипты из Index.cshtml

--- src/NoteApp/NoteApp/wwwroot/js/site.js
+++ src/NoteApp/NoteApp/wwwroot/js/site.js
@@ -1,4 +1,21 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+$(function () {
+    $('select#NotesListBox').change(function () {
+        var noteId = $(this).val();
 
-// Write your JavaScript code.
+        $.ajax({
+            url: '/Home/LoadSelectedNote/',
+            type: "POST",
+            data: {"noteId":noteId},
+            success: function (result) {
+                console.log(result);
+                $('h4#Title').text(result.name);
+                $('p#Category').text(result.category);
+                $('input#TimeOfCreate').val(result.timeOfCreate);
+                $('input#TimeOfUpdate').val(result.timeOfUpdate);
+                $('textarea#Description').val(result.description)
+            }
+        });
+    });
+});
