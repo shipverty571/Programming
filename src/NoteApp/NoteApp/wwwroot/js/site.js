0d0c7416efcbb0ebc2a838fc1e17a8d9888b350d
Валентин Борнашов <shipverty@vk.com>
2023-09-28 17:26:04 +0700

    refactor(site.js): убран метод из-за использования формы в EditNote.cshtml

--- src/NoteApp/NoteApp/wwwroot/js/site.js
+++ src/NoteApp/NoteApp/wwwroot/js/site.js
@@ -20,7 +20,6 @@ $(function () {
             type: "POST",
             data: {"noteId":noteId},
             success: function (result) {
-                console.log(result);
                 $('h4#Title').text(result.name);
                 $('p#Category').text(result.category);
                 $('input#TimeOfCreate').val(result.timeOfCreate);
@@ -46,35 +45,6 @@ $(function () {
 $(function () {
     $('button#CancelAddNoteButton').on('click', function (e) {
         window.location.href = window.location.origin;
-    });
-});
-
-/**
- * При вызове добавляет заметку в базу данных.
- */
-$(function () {
-    $('button#AddNoteButton').on('click', function (e) {
-        var name = $('input#Name').val();
-        var category = $('select#Category').val();
-        var description = $('textarea#Description').val();
-        var id = $("div.main").attr('id');
-        console.log(name);
-        console.log(category);
-        console.log(description);
-        console.log(id);
-        $.ajax({
-            url: '/Home/Index/',
-            type: "POST",
-            data: {
-                "Id": id,
-                "Name": name,
-                "Category": category,
-                "Description": description
-            },
-            success: function (result) {
-                window.location.href = window.location.origin;
-            }
-        });
     });
 });
 
