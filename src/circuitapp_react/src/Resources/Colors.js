a741cd53602743f565aedc56c974d612c2d26ed9
Валентин Борнашов <shipverty@vk.com>
2023-11-14 22:25:14 +0700

    feature(Colors.js, Values.js): вынес константные значения в отдельные ресурсные файлы

---
+++ src/circuitapp_react/src/Resources/Colors.js
@@ -0,0 +1,13 @@
+﻿/**
+ * Цвет элемента при его передвижении.
+ * @type {string}
+ * @const
+ */
+export const DraggableColor = "gray";
+
+/**
+ * Цвет элемента, когда он не передвигается.
+ * @type {string}
+ * @const
+ */
+export const NoDraggableColor = "black";
---
+++ src/circuitapp_react/src/Resources/Values.js
@@ -0,0 +1,13 @@
+﻿/**
+ * Базовая ширина рамки выбранного элемента.
+ * @type {number}
+ * @const
+ */
+export const SelectedStrokeWidth = 2;
+
+/**
+ * Базовое число ширины прерывистой линии.
+ * @type {number}
+ * @const
+ */
+export const SelectedStrokeDashArray = 8;
