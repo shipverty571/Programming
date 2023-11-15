19a0e3f360c07323966e8495576e6663893db8a7
Валентин Борнашов <shipverty@vk.com>
2023-11-15 22:53:24 +0700

    feature(CanvasPages.js, PageButton.js, App.css): добавлен компонент кнопки страницы

--- src/circuitapp_react/src/components/Canvas/CanvasPages.js
+++ src/circuitapp_react/src/components/Canvas/CanvasPages.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PageButton from "./PageButton";
 
 /**
  * Компонент страниц канваса.
@@ -8,12 +9,21 @@ class CanvasPages extends Component {
         return (
             <div className='horizontal-content' style={{ justifyContent: 'space-between', backgroundColor: '#F3F3F3' }}>
                 <fieldset className='horizontal-content' id='pages-group'>
-                    <input type='radio' value='1' name='pages-group' id='Page1' />
-                    <label htmlFor='Page1'>Page 1</label>
-                    <input type='radio' value='2' name='pages-group' id='Page2' />
-                    <label htmlFor='Page2'>Page 2</label>
-                    <input type='radio' value='3' name='pages-group' id='Page3' />
-                    <label htmlFor='Page3'>Page 3</label>
+                    <PageButton id="000111" name="Page 1111" />
+                    <PageButton id="0001121" name="Page 1111" />
+                    <PageButton id="0001311" name="Page 1111" />
+                    {/*<div>
+                        <input type='radio' value='1' name='pages-group' id='Page1' />
+                        <label htmlFor='Page1'>Page 1</label>
+                    </div>
+                    <div>
+                        <input type='radio' value='2' name='pages-group' id='Page2' />
+                        <label htmlFor='Page2'>Page 2</label>
+                    </div>
+                    <div>
+                        <input type='radio' value='3' name='pages-group' id='Page3' />
+                        <label htmlFor='Page3'>Page 3</label>
+                    </div>*/}
                 </fieldset>
                 <div id='edit-pages-count-div'>
                     <button id='add-page-button' />
---
+++ src/circuitapp_react/src/components/Canvas/PageButton.js
@@ -0,0 +1,17 @@
+﻿import React, {Component} from 'react';
+
+class PageButton extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <input type='radio' value={this.props.id} name='pages-group' id={this.props.id} />
+                <label htmlFor={this.props.id}>{this.props.name}</label>
+            </div>
+        );
+    }
+}
+
+export default PageButton;
