8217d91885a795164911f66b216c0e945ec1f14c
Валентин Борнашов <shipverty@vk.com>
2023-11-24 11:20:42 +0700

    feature(App.css): изменен стиль блока со страницами

--- src/circuitapp_react/src/components/Canvas/CanvasPages.js
+++ src/circuitapp_react/src/components/Canvas/CanvasPages.js
@@ -7,24 +7,24 @@ import PageButton from "./PageButton";
 class CanvasPages extends Component {
     render() {
         return (
-            <div className='horizontal-content' style={{ justifyContent: 'space-between', backgroundColor: '#F3F3F3', width: '100%' }}>
-                <div style={{width: '100vw', maxWidth: '100%'}}>
-                    <div id='pages-group'>
-                        {this.props.pages.map(page =>
-                            <PageButton
-                                key={page.id}
-                                id={page.id}
-                                name={page.name}
-                                activePageId={this.props.activePageId}
-                                setActivePage={this.props.setActivePage}
-                            />)}
-                    </div>
-                </div>
-                
-                
+            <div className='horizontal-content' id='canvas-pages'>
                 <div id='edit-pages-count-div'>
                     <button id='add-page-button' onClick={() => this.props.onAddPage()} />
-                    <button id='remove-page-button' onClick={() => this.props.onRemovePage()} disabled={this.props.canNotRemovePage} />
+                    <button 
+                        id='remove-page-button' 
+                        onClick={() => this.props.onRemovePage()} 
+                        disabled={this.props.canNotRemovePage} 
+                    />
+                </div>
+                <div id='pages-group'>
+                    {this.props.pages.map(page =>
+                        <PageButton
+                            key={page.id}
+                            id={page.id}
+                            name={page.name}
+                            activePageId={this.props.activePageId}
+                            setActivePage={this.props.setActivePage}
+                        />)}
                 </div>
             </div>
         );
